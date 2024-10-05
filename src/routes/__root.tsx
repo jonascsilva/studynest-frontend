4639db4d1ed8f5d1d64cf799b7b10e97d49a15b7
jonascsilva <55038425+jonascsilva@users.noreply.github.com>
2024-10-05 20:58:25 -0300

    update packages

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,13 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import type { QueryClient } from '@tanstack/react-query'
 
-export const Route = createRootRoute({
+interface MyRouterContext {
+  queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
       <Outlet />
