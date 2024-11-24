b836014a623b1bd7924de53e0dfb2e21cb18ba0f
jonascsilva <55038425+jonascsilva@users.noreply.github.com>
2024-11-24 20:49:26 +0000

    create deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,52 +5,53 @@ import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      $: path.resolve(__dirname, './src')
-    }
-  },
-  plugins: [TanStackRouterVite(), react()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
-    browser: {
-      name: 'firefox',
-      headless: false,
-      viewport: {
-        height: 1080,
-        width: 1920
+export default defineConfig(() => {
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 80
+
+  return {
+    resolve: {
+      alias: {
+        $: path.resolve(__dirname, './src')
       }
     },
-    coverage: {
-      reporter: ['text', 'lcov'],
-      exclude: [
-        '**/main.tsx',
-        '**/router.tsx',
-        '**/routeTree.gen.ts',
-        '**/__root.tsx',
-        '**/App.tsx',
-        '**/router.ts',
-        '**/types/**',
-        ...coverageConfigDefaults.exclude
-      ]
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler'
+    plugins: [TanStackRouterVite(), react()],
+    test: {
+      environment: 'jsdom',
+      setupFiles: ['./vitest.setup.ts'],
+      browser: {
+        name: 'firefox',
+        headless: false,
+        viewport: {
+          height: 1080,
+          width: 1920
+        }
+      },
+      coverage: {
+        exclude: [
+          '**/main.tsx',
+          '**/router.tsx',
+          '**/routeTree.gen.ts',
+          '**/__root.tsx',
+          '**/App.tsx',
+          '**/router.ts',
+          '**/types/**',
+          ...coverageConfigDefaults.exclude
+        ]
       }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler'
+        }
+      }
+    },
+    server: {
+      host: true
+    },
+    preview: {
+      port,
+      host: true
     }
-  },
-  server: {
-    port: 5173,
-    host: true
-  },
-  preview: {
-    port: 8080,
-    host: true
   }
 })
